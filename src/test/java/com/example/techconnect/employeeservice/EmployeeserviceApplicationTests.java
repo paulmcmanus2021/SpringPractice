9d13df3f7b505196b6bfacfa5cb7d21b937bab2e
Paul McManus <pmcmanus2021@gmail.com>
2020-07-15 12:13:18 +0100

    Init commit, Spring Practice. SpringBoot started, entering models.

---
+++ src/test/java/com/example/techconnect/employeeservice/EmployeeserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.techconnect.employeeservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
